ac8d6342fc3a52a26eee025ed43cc2da4eabb445
Alan Neres <alanneres10@hotmail.com>
2020-04-19 17:36:04 -0300

    Projeto Completo

---
+++ BuscadorCEP/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace BuscadorCEP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLogradouro = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCep = new System.Windows.Forms.MaskedTextBox();
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblEstado);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.lblCidade);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblBairro);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblLogradouro);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtCep);
+            this.panel1.Controls.Add(this.lblCEP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 420);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblEstado.ForeColor = System.Drawing.Color.DimGray;
+            this.lblEstado.Location = new System.Drawing.Point(101, 373);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 23);
+            this.lblEstado.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(76, 350);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Estado";
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblCidade.ForeColor = System.Drawing.Color.DimGray;
+            this.lblCidade.Location = new System.Drawing.Point(59, 306);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(0, 23);
+            this.lblCidade.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(81, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cidade";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblBairro.ForeColor = System.Drawing.Color.DimGray;
+            this.lblBairro.Location = new System.Drawing.Point(58, 240);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(0, 23);
+            this.lblBairro.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(84, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bairro";
+            // 
+            // lblLogradouro
+            // 
+            this.lblLogradouro.AutoSize = true;
+            this.lblLogradouro.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblLogradouro.ForeColor = System.Drawing.Color.DimGray;
+            this.lblLogradouro.Location = new System.Drawing.Point(58, 178);
+            this.lblLogradouro.Name = "lblLogradouro";
+            this.lblLogradouro.Size = new System.Drawing.Size(0, 23);
+            this.lblLogradouro.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(58, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Logradouro";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCep
+            // 
+            this.txtCep.Font = new System.Drawing.Font("Roboto", 14.25F);
+            this.txtCep.Location = new System.Drawing.Point(64, 52);
+            this.txtCep.Mask = "00000-000";
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(100, 30);
+            this.txtCep.TabIndex = 1;
+            this.txtCep.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCep_KeyPress);
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblCEP.Location = new System.Drawing.Point(41, 26);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(148, 23);
+            this.lblCEP.TabIndex = 0;
+            this.lblCEP.Text = "Digite seu CEP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 420);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BuscadorCEP";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCEP;
+        private System.Windows.Forms.MaskedTextBox txtCep;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLogradouro;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BuscadorCEP/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Refit;
+using System;
+using System.Windows.Forms;
+
+namespace BuscadorCEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public async void resultApi()
+        {
+            try
+            {
+                if (txtCep.MaskCompleted == true)
+                {
+                    var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                    var address = await cepClient.GetAddressAsync(txtCep.Text);
+                    lblLogradouro.Text = address.Logradouro;
+                    lblBairro.Text = address.Bairro;
+                    lblCidade.Text = address.Localidade;
+                    lblEstado.Text = address.Uf;
+                }
+                else
+                {
+                    MessageBox.Show("Digite um CEP Válido!");
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Deu o seguinte error: " + Ex);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultApi();
+        }
+
+        private void txtCep_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Se a tecla digitada não for número
+            if (!char.IsDigit(e.KeyChar))
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == (char)13)
+            {
+                resultApi();
+            }
+        }
+    }
+}
